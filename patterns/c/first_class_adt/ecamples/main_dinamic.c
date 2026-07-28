39ae09bf5f2859c896221f5ebbb65dfbf4a6de6b
Ivan Lopes <lopesivan.ufrj@gmail.com>
2026-07-28 04:57:01 -0300

    :add: Adiciona o arquivo patterns/c/first_class_adt/ecamples/main_dinamic.c

---
+++ patterns/c/first_class_adt/ecamples/main_dinamic.c
@@ -0,0 +1,9 @@
+// Gerado com: prefix=app, adt_name=Customer, allocation_strategy=dynamic
+#include "app_Customer.h"
+
+int main() {
+  app_CustomerPtr customer = app_Customer_create(/* params */);
+  // Use customer...
+  app_Customer_destroy(customer);
+  return 0;
+}
